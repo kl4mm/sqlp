0bca95b1f80210abd43ae1ec76ec80b1003d6dd9
Subhan <s.ashraf9@protonmail.com>
2024-02-04 19:21:39 +0000

    parse more expressions

--- src/parse2.rs
+++ src/parse2.rs
@@ -11,6 +11,11 @@ pub enum Op {
 
     Conjunction,
     Disjunction,
+
+    Negation,
+    In,
+    Between,
+    Is,
 }
 
 impl std::fmt::Display for Op {
@@ -24,6 +29,10 @@ impl std::fmt::Display for Op {
             Op::Ge => write!(f, ">="),
             Op::Conjunction => write!(f, "AND"),
             Op::Disjunction => write!(f, "OR"),
+            Op::Negation => write!(f, "NOT"),
+            Op::In => write!(f, "IN"),
+            Op::Between => write!(f, "BETWEEN"),
+            Op::Is => write!(f, "IS"),
         }
     }
 }
@@ -39,6 +48,10 @@ impl From<Token> for Op {
             Token::Le => Op::Le,
             Token::Gt => Op::Gt,
             Token::Ge => Op::Ge,
+            Token::Negation => Op::Negation,
+            Token::In => Op::In,
+            Token::Between => Op::Between,
+            Token::Is => Op::Is,
 
             _ => unreachable!(),
         }
@@ -56,7 +69,10 @@ impl From<Op> for Token {
             Op::Ge => Token::Ge,
             Op::Conjunction => Token::Conjunction,
             Op::Disjunction => Token::Disjunction,
-            _ => unreachable!(),
+            Op::Negation => Token::Negation,
+            Op::In => Token::In,
+            Op::Between => Token::Between,
+            Op::Is => Token::Is,
         }
     }
 }
@@ -97,6 +113,7 @@ pub enum Node {
     IntegerLiteral(u64),
 
     All,
+    Null,
 
     None,
 }
@@ -143,6 +160,7 @@ impl std::fmt::Display for Node {
             Node::StringLiteral(s) => write!(f, "\"{s}\""),
             Node::IntegerLiteral(i) => write!(f, "{i}"),
             Node::All => write!(f, "*"),
+            Node::Null => write!(f, "NULL"),
             Node::None => todo!(),
         }
     }
@@ -237,6 +255,54 @@ fn column_list(l: &mut Lexer) -> Result<Vec<Node>> {
     column_list(l, Vec::new(), State::Column)
 }
 
+fn list(l: &mut Lexer) -> Result<Vec<Node>> {
+    #[derive(PartialEq, Debug)]
+    enum State {
+        Comma,
+        Item,
+    }
+
+    fn integer_list(l: &mut Lexer, mut list: Vec<Node>, state: State) -> Result<Vec<Node>> {
+        match l.peek() {
+            Token::Comma if state == State::Comma => {
+                l.next();
+                integer_list(l, list, State::Item)
+            }
+            Token::IntegerLiteral(i) if state == State::Item => {
+                l.next();
+                list.push(Node::IntegerLiteral(i));
+                integer_list(l, list, State::Comma)
+            }
+
+            _ if state == State::Comma => return Ok(list),
+            t => Err(Unexpected(t)),
+        }
+    }
+
+    fn string_list(l: &mut Lexer, mut list: Vec<Node>, state: State) -> Result<Vec<Node>> {
+        match l.peek() {
+            Token::Comma if state == State::Comma => {
+                l.next();
+                string_list(l, list, State::Item)
+            }
+            Token::StringLiteral(s) if state == State::Item => {
+                l.next();
+                list.push(Node::StringLiteral(s));
+                string_list(l, list, State::Comma)
+            }
+
+            _ if state == State::Comma => return Ok(list),
+            t => Err(Unexpected(t)),
+        }
+    }
+
+    match l.peek() {
+        Token::IntegerLiteral(_) => integer_list(l, Vec::new(), State::Item),
+        Token::StringLiteral(_) => string_list(l, Vec::new(), State::Item),
+        t => Err(Unexpected(t)),
+    }
+}
+
 fn column_ref(l: &mut Lexer) -> Result<Node> {
     match l.next() {
         Token::TableOrColumnReference(column) => Ok(Node::ColumnRef {
@@ -294,7 +360,7 @@ fn parens<T, F: Fn(&mut Lexer) -> Result<T>>(l: &mut Lexer, f: F) -> Result<T> {
     let n = f(l)?;
 
     match l.next() {
-        Token::LParen => {}
+        Token::RParen => {}
         t => Err(Unexpected(t))?,
     }
 
@@ -327,7 +393,22 @@ fn expr(l: &mut Lexer) -> Result<Node> {
             },
             Token::StringLiteral(s) => Node::StringLiteral(s),
             Token::IntegerLiteral(i) => Node::IntegerLiteral(i),
-            // TODO: NOT expr
+            Token::Null => Node::Null,
+            t if is_prefix(&t) => {
+                let op = t.into();
+                if op == Op::Between {
+                    // Between is binary but its operator is prefix
+                    between(l)?
+                } else if op == Op::In {
+                    // In is n-ary but its operator is prefix
+                    Node::Expr(op, parens(l, list)?)
+                } else {
+                    let ((), r_bp) = prefix_bp(&op);
+                    let rhs = expr_bp(l, r_bp)?;
+                    Node::Expr(op, vec![rhs])
+                }
+            }
+            // TODO: NOT expr (covered by prefix?)
             t => Err(Unexpected(t))?,
         };
 
@@ -355,6 +436,25 @@ fn expr(l: &mut Lexer) -> Result<Node> {
     expr_bp(l, 0)
 }
 
+fn between(l: &mut Lexer) -> Result<Node> {
+    let from = match l.next() {
+        Token::IntegerLiteral(i) => Node::IntegerLiteral(i),
+        t => Err(Unexpected(t))?,
+    };
+
+    match l.next() {
+        Token::Conjunction => {}
+        t => Err(Unexpected(t))?,
+    };
+
+    let to = match l.next() {
+        Token::IntegerLiteral(i) => Node::IntegerLiteral(i),
+        t => Err(Unexpected(t))?,
+    };
+
+    Ok(Node::Expr(Op::Between, vec![from, to]))
+}
+
 fn is_infix(t: &Token) -> bool {
     match t {
         Token::Conjunction
@@ -369,10 +469,25 @@ fn is_infix(t: &Token) -> bool {
     }
 }
 
-fn infix_bp(t: &Op) -> (u8, u8) {
+fn is_prefix(t: &Token) -> bool {
     match t {
+        Token::Negation | Token::In | Token::Between | Token::Is => true,
+        _ => false,
+    }
+}
+
+fn infix_bp(op: &Op) -> (u8, u8) {
+    match op {
         Op::Conjunction | Op::Disjunction => (1, 2),
         Op::Eq | Op::Neq | Op::Lt | Op::Le | Op::Gt | Op::Ge => (3, 4),
+        _ => unreachable!(),
+    }
+}
+
+fn prefix_bp(op: &Op) -> ((), u8) {
+    match op {
+        Op::Negation | Op::Is => ((), 5),
+        Op::In | Op::Between => unreachable!("IN and BETWEEN are parsed without binding power"),
         _ => unreachable!(),
     }
 }
@@ -484,12 +599,40 @@ mod test {
                 input: "12 < 14 AND 14 > 12 OR name != \"bob\";",
                 want: "(OR (AND (< 12 14) (> 14 12)) (!= name \"bob\"))",
             },
+            Test {
+                input: "NULL;",
+                want: "NULL",
+            },
+            Test {
+                input: "IS NULL;",
+                want: "(IS NULL)",
+            },
+            Test {
+                input: "NOT NULL;",
+                want: "(NOT NULL)",
+            },
+            Test {
+                input: "NOT BETWEEN 100 AND 200;",
+                want: "(NOT (BETWEEN 100 200))",
+            },
+            Test {
+                input: "NOT BETWEEN 100 AND 200 AND 1 < 2;",
+                want: "(AND (NOT (BETWEEN 100 200)) (< 1 2))",
+            },
+            Test {
+                input: "NOT IN (1, 2, 3, 4);",
+                want: "(NOT (IN 1 2 3 4))",
+            },
+            Test {
+                input: "NOT IN (1, 2, 3, 4) AND columna = columnb OR IN (6, 7, 8);",
+                want: "(OR (AND (NOT (IN 1 2 3 4)) (= columna columnb)) (IN 6 7 8))",
+            },
         ];
 
         for Test { input, want } in tcs {
             let mut l = Lexer::new(input);
             let have = expr(&mut l)?.to_string();
-            assert_eq!(want, have, "Want: {want}\nHave: {have}");
+            assert_eq!(want, have);
         }
 
         Ok(())
